9f58af5a156fc43b53f9f8b6cc15bcfb48809e9c
Eduardo C <edu_ce_1988@hotmail.com>
2017-07-22 11:45:35 +0200

    SERVICE: change the method creation of fake data for a call to the new injected  variable method getCustomers() from customerDAO DAO: access to the DB and get the results sorted by last name using a query with the class and the variables of our Customer entity           using the session from the injected variable SessionFactory created in spring-mvc-servlet (spring configuration file) SPRING config: creation of data source with the connection pool properties, session factory and the transaction manager enabling the annotations (for @Transactional)

--- web-app-spring-mvc/src/com/eduardo/springdemo/service/CustomerServiceImpl.java
+++ web-app-spring-mvc/src/com/eduardo/springdemo/service/CustomerServiceImpl.java
@@ -1,33 +1,24 @@
 package com.eduardo.springdemo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eduardo.springdemo.dao.CustomerDAO;
 import com.eduardo.springdemo.entity.Customer;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+	// CustomerDAO injected
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@Override
+	@Transactional
 	public List<Customer> getCustomers() {
-
-		List<Customer> customers = new ArrayList<Customer>(){{
-			add(getFakeCustomer(1));
-			add(getFakeCustomer(2));
-			add(getFakeCustomer(3));
-			add(getFakeCustomer(4));
-		}};
-		return customers;
+		return customerDAO.getCustomers();
 	}
-
-	protected Customer getFakeCustomer(int number) {
-		Customer fakeCustomer = new Customer();
-		fakeCustomer.setFirstName("John" + number);
-		fakeCustomer.setLastName("Doe" + number);
-		fakeCustomer.setEmail("johndoe@johndoe.com");
-		return fakeCustomer;
-	}
-
 }
